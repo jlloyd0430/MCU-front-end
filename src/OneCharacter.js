bbfbc32126e0250c17718b336cbdf776de51b294
jesse Lloyd <jesselloyd911@gmail.com>
2023-01-31 12:29:12 -0500

    client refactor

--- src/OneCharacter.js
+++ src/OneCharacter.js
@@ -1,24 +1,92 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function OneCharacter() {
-    const { name } = useParams();
+  const { name } = useParams();
 
-    const [character, setCharacter] = useState({})
+  const [character, setCharacter] = useState({});
+  const [isEditing, setIsEditing] = useState(false);
+  const [debutInput, setDebutInput] = useState("");
+  const [debutYearInput, setDebutYearInput] = useState("");
 
-    useEffect(() => {
-        fetch(`http://localhost:3001/api/getCharacterByName/${name}`)
-            .then(async res => {
-                let result = await res.json()
-                setCharacter(result.payload)
-            })
-    }, [name])
-
-    return (  
-        <>
-            <h1>The character {character.name} debuted in the film {character.debut} that was released in {character.debutYear}</h1>
-        </>
+  useEffect(() => {
+    fetch(`http://localhost:3001/api/getCharacterByName/${name}`).then(
+      async (res) => {
+        let result = await res.json();
+        setCharacter(result.payload);
+        setDebutInput(result.payload.debut);
+        setDebutYearInput(result.payload.debutYear);
+      }
     );
+  }, [name, isEditing]);
+
+  function toggleEditing() {
+    isEditing ? setIsEditing(false) : setIsEditing(true);
+  }
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+
+    console.log("Submitted!");
+
+    let sendBody = {
+      debut: debutInput,
+      debutYear: debutYearInput,
+    };
+
+    fetch(`http://localhost:3001/api/updateCharacter/${character._id}`, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": "token-value",
+      },
+      body: JSON.stringify(sendBody),
+    }).then(() => {
+      setIsEditing(false);
+    });
+  }
+
+  return (
+    <>
+      <h1>The character {character.name}</h1>
+      <ul>
+        <form onSubmit={handleOnSubmit}>
+          <li>
+            debuted in &nbsp;
+            {isEditing ? (
+              <input
+                type="text"
+                name="debut"
+                value={debutInput}
+                onChange={(e) => setDebutInput(e.target.value)}
+              />
+            ) : (
+              <span>{character.debut}</span>
+            )}
+            .
+          </li>
+          <li>
+            This was released in &nbsp;
+            {isEditing ? (
+              <input
+                type="text"
+                name="debutYear"
+                value={debutYearInput}
+                onChange={(e) => setDebutYearInput(e.target.value)}
+              />
+            ) : (
+              <span>{character.debutYear}</span>
+            )}
+            .
+          </li>
+          {isEditing ? <button type="submit">Submit edit</button> : <br />}
+        </form>
+      </ul>
+      <button onClick={toggleEditing}>
+        {isEditing ? "Stop editing" : "Edit character details"}
+      </button>
+    </>
+  );
 }
 
 export default OneCharacter;
